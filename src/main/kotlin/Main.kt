c890db2ada7905bf8dddafb8c48dbfdfbe7cb7c1
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2025-10-08 13:43:17 +0300

    When expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+    println("Please enter a number: ")
+    val input = readln()
+    val inputAsInteger= input.toIntOrNull()
+
+    val output=when(inputAsInteger){
+        null ->"Dude enter a valid numbe!"
+        3->"The number is three"
+        5->"The number is five"
+        in 4..20 -> "The number is between 10 and 20"
+        else -> "I have no ideea what to print"
+    }
+    println(output)
 }

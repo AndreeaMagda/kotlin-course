e5c45d73699eb0fd9bd8245de7cfca2f05dd4fc6
Andreea Magda <122922300+AndreeaMagda@users.noreply.github.com>
2025-10-20 15:07:02 +0300

    Lambda part 2

--- src/main/kotlin/Lambda.kt
+++ src/main/kotlin/Lambda.kt
@@ -4,14 +4,19 @@ fun main() {
     println("Enter a string")
     val input = readln()
 
-    val lettersOnly = input.myFilter { it.isLetter() }
+    val favoriteNumbers = intArrayOf(1, 52, 23, 43, 51)
+    favoriteNumbers.map { it * it }
+
+    val lettersOnly = input.myFilter {
+        this.isLetter()
+    }
     println(lettersOnly)
 }
 
-fun String.myFilter(predicate: (Char) -> Boolean): String {
+fun String.myFilter(predicate: Char.() -> Boolean): String {
     return buildString {
         for (char in this@myFilter) {
-            if (predicate(char)) {
+            if (char.predicate()) {
                 append(char)
             }
         }
